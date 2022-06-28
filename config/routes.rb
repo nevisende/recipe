790b16a227e5fe427db4ae1e0148e3ca58c200f3
nevisende <ekonomitakipmail@gmail.com>
2022-06-28 07:32:55 +0300

    Feat: make the login page the root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    authenticated :user do
+      root :to => "users#show", as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
 end
