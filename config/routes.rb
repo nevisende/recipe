f5a286c1dc19269fad45a7e4f18bd0d578c55489
nevisende <ekonomitakipmail@gmail.com>
2022-07-01 10:18:34 +0300

    Feat: add food link to the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'foods#index'
   get 'users/index'
   get 'users/sign_out'
   resources :users, only: %i[index]
+  resources :foods, only: %i[index new create destroy]
+
 
   get '/public_recipes', to: 'recipes#public'
   resources :recipes, only: %i[index show new create destroy]
