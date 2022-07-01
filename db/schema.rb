62ffea16f5e6b1e33dddc1021558fd4b93a4157f
nevisende <ekonomitakipmail@gmail.com>
2022-07-01 11:20:07 +0300

    Fix: creation food problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_082656) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_01_081519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
-    t.string "measurement_unit"
-    t.float "price"
-    t.integer "quantity"
-    t.bigint "user_id", null: false
+    t.string "measurementUnit"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
